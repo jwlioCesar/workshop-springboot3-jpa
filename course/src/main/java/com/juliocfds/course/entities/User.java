791f0464da7d69ac03a98c99633910703c036295
Júlio César <juliocfds20@gmail.com>
2023-05-02 15:47:10 -0300

    H2 database, teste profile, JPA

--- course/src/main/java/com/juliocfds/course/entities/User.java
+++ course/src/main/java/com/juliocfds/course/entities/User.java
@@ -3,12 +3,23 @@ package com.juliocfds.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	//Serializable é uma interface é útil quando precisamos 
 	//enviar objetos pela rede, salvar no disco, ou comunicar de uma JVM com outra.
 	
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
