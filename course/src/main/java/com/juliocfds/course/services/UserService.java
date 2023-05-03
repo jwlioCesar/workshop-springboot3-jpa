dd32c2d9a718f802a553f13ee065b14708463a21
Júlio César <juliocfds20@gmail.com>
2023-05-03 16:23:11 -0300

    Service Layer, componet registration

---
+++ course/src/main/java/com/juliocfds/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.juliocfds.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juliocfds.course.entities.User;
+import com.juliocfds.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
