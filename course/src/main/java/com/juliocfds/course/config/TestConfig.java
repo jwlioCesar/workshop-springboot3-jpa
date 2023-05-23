0725d0991c0fd404ada53ab1227c672b2086e279
Júlio César <juliocfds20@gmail.com>
2023-05-23 12:19:43 -0300

    Payment, one-to-one association

--- course/src/main/java/com/juliocfds/course/config/TestConfig.java
+++ course/src/main/java/com/juliocfds/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.juliocfds.course.entities.Category;
 import com.juliocfds.course.entities.Order;
 import com.juliocfds.course.entities.OrderItem;
+import com.juliocfds.course.entities.Payment;
 import com.juliocfds.course.entities.Product;
 import com.juliocfds.course.entities.User;
 import com.juliocfds.course.enums.OrderStatus;
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
